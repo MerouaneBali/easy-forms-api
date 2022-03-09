0468468f1c35c9a068ec3d418d077820e1b24c06
Merouane Bali <merouane.bali.inbox@gmail.com>
2022-03-09 22:11:04 +0100

    fix: remove forms route console logs

--- routes/forms.js
+++ routes/forms.js
@@ -40,8 +40,6 @@ const formsValidationSchemas = require("../validation/formsValidationSchemas");
 router.post("/", authorized, async (req, res) => {
   const { Project, Form } = require("../models");
 
-  console.log("object");
-
   const body = {
     name: req.body.name,
     project: req.body.project,
@@ -51,8 +49,6 @@ router.post("/", authorized, async (req, res) => {
   const { error, value } = formsValidationSchemas.create.validate(body);
 
   if (error) {
-    console.log(error);
-
     const validationError = error.details[0];
 
     return res.status(400).json({
@@ -113,8 +109,6 @@ router.post(
     origin: ["*"],
   }),
   async (req, res) => {
-    console.log(req.body);
-
     const { Form } = require("../models");
 
     const { formId, projectId } = req.params;
@@ -128,23 +122,8 @@ router.post(
         { $push: { inbox: { data: { ...req.body } } } }
       );
 
-      // const projects = result.map((project) => {
-      //   project.forms.map((form) => {
-      //     console.log(form.id, form.inbox);
-      //     // form.count = form.inbox.length;
-      //     return form;
-      //   });
-
-      //   // console.log(forms[0].inbox);
-      // });
-
-      // console.log(projects);
-
-      // console.log(req.params.formId, result);
-
       res.sendStatus(200);
     } catch (error) {
-      console.log(error);
       return res.sendStatus(500);
     }
   }
@@ -157,21 +136,11 @@ router.patch(
     origin: ["*"],
   }),
   async (req, res) => {
-    console.log(req.body);
-
     const { Form } = require("../models");
 
     const { formId, messageId } = req.params;
 
     try {
-      // await Form.findOneAndUpdate(
-      //   {
-      //     _id: formId,
-      //     "inbox.id": String(messageId),
-      //   },
-      //   { $set: { "inbox.$.resolved": String(true) } }
-      // ).then((doc) => console.log(doc));
-
       const form = await Form.findById(formId);
 
       const message = await form.inbox.id(messageId);
@@ -182,7 +151,6 @@ router.patch(
 
       res.sendStatus(200);
     } catch (error) {
-      console.log(error);
       return res.sendStatus(500);
     }
   }
@@ -195,21 +163,11 @@ router.patch(
     origin: ["*"],
   }),
   async (req, res) => {
-    console.log(req.body);
-
     const { Form } = require("../models");
 
     const { formId, messageId } = req.params;
 
     try {
-      // await Form.findOneAndUpdate(
-      //   {
-      //     _id: formId,
-      //     "inbox.id": String(messageId),
-      //   },
-      //   { $set: { "inbox.$.resolved": String(true) } }
-      // ).then((doc) => console.log(doc));
-
       const form = await Form.findById(formId);
 
       const message = await form.inbox.id(messageId);
@@ -217,13 +175,10 @@ router.patch(
       message.opened = true;
       message.resolved = true;
 
-      console.log(message);
-
       await form.save();
 
       res.sendStatus(200);
     } catch (error) {
-      console.log(error);
       return res.sendStatus(500);
     }
   }
@@ -236,8 +191,6 @@ router.delete(
     origin: ["*"],
   }),
   async (req, res) => {
-    console.log(req.body);
-
     const { Form } = require("../models");
 
     const { formId, messageId } = req.params;
@@ -248,11 +201,10 @@ router.delete(
           _id: formId,
         },
         { $pull: { inbox: { _id: messageId } } }
-      ).then((doc) => console.log(doc));
+      ).then((doc) => doc);
 
       res.sendStatus(200);
     } catch (error) {
-      console.log(error);
       return res.sendStatus(500);
     }
   }
@@ -272,27 +224,6 @@ router.get("/:projectId/:formId", authorized, async (req, res) => {
       },
       "name project inbox"
     ).populate("project", "name");
-
-    // const result = await Form.findOne(
-    //   {
-    //     _id: formId,
-    //     project: projectId,
-    //     author: req.user.id,
-    //   },
-    //   "name project inbox"
-    // );
-
-    // const projects = result.map((project) => {
-    //   project.forms.map((form) => {
-    //     console.log(form.id, form.inbox);
-    //     // form.count = form.inbox.length;
-    //     return form;
-    //   });
-
-    //   // console.log(forms[0].inbox);
-    // });
-
-    // console.log(projects);
 
     return result ? res.status(200).send(result) : res.sendStatus(404);
   } catch (error) {
